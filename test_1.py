f274117fb8572bdfce576ed847e9f5e515dcf185
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-06-18 18:18:03 -0700

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,9 +1,4 @@
 import task
 
 def test_1_1():
-    assert task.hello() == "Hello, World!"
-
-def test_1_2():
-    assert task.add(2, 3) == 5
-    assert task.add(-2, 3) == 1
-    assert task.add("Hello, ", "World!") == "Hello, World!"
+    assert task.operations() == 5007, 6251206725228478614
